136046026bf57ccb4e6a9358dd0221a016c9b229
Samuel <sly@ou.edu>
2023-04-01 17:23:33 -0500

    added script to scan and read aloud 'english characters

---
+++ translator.py
@@ -0,0 +1,24 @@
+import cv2
+import pytesseract
+from gtts import gTTS
+import os
+
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cam_port = 0
+cam = cv2.VideoCapture(cam_port)
+result, image = cam.read()
+
+if result:
+	cv2.imwrite("scannedImage.jpg", image)
+else:
+	print("Image detection error")
+
+image = cv2.imread('scannedImage.jpg')
+translation = pytesseract.image_to_string(image)
+print(translation)
+gttsObj = gTTS(text=translation, lang='en', slow=False)
+gttsObj.save("translation.mp3")
+  
+os.system("start translation.mp3")
