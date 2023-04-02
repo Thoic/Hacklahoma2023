e7cf6f55e873dbea97beb2d7c9aa96874aaa5f6b
Samuel <sly@ou.edu>
2023-04-02 03:21:18 -0500

    added boxes and lowercase conversion

--- translator.py
+++ translator.py
@@ -8,6 +8,8 @@ def get_grayscale(image):
 
 cam_port = 0
 cam = cv2.VideoCapture(cam_port)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 result, image = cam.read()
 
 if result:
@@ -16,7 +18,17 @@ else:
 	print("Image detection error")
 
 image = cv2.imread('scannedImage.jpg')
-translation = pytesseract.image_to_string(image)
+
+h, w, c = image.shape
+boxes = pytesseract.image_to_boxes(image) 
+for b in boxes.splitlines():
+    b = b.split(' ')
+    img = cv2.rectangle(image, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
+
+cv2.imshow('img', image)
+cv2.waitKey(0)
+
+translation = pytesseract.image_to_string(image).lower()
 print(translation)
 if(translation == ""):
 	translation = "No text detected"
